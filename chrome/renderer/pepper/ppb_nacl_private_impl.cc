nacl-webkit/chrome_deps

commit c6c34d9d6949b65000e124344f8470fa1c5d82ff

Set the nacl pipe to non-blocking

chrome/renderer/pepper/ppb_nacl_private_impl.cc
@@ -132,6 +132,7 @@ PP_NaClResult LaunchSelLdr(PP_Instance instance,
     DLOG(ERROR) << "WebKit::WebProcess::shared().launchNaCl() failed";
     return PP_NACL_FAILED;
   }
+  ASSERT(fcntl(result_socket.fd, F_SETFL, O_NONBLOCK) != -1);
   ASSERT(fcntl(instance_info.channel_handle.socket.fd, F_SETFL, O_NONBLOCK) != -1);
 
   // Don't save instance_info if channel handle is invalid.

ipc/ipc_channel_posix.cc
@@ -381,8 +381,10 @@ bool Channel::ChannelImpl::CreatePipe(
       return false;
     }
     if (!(value & O_NONBLOCK)) {
-      LOG(ERROR) << "Socket " << pipe_name_ << " must be O_NONBLOCK";
-      return false;
+      DCHECK(fcntl(local_pipe, F_SETFL, O_NONBLOCK) != -1);
+      LOG(INFO) << "Socket " << pipe_name_ << " must be O_NONBLOCK"<<" ... so we set it to non-blocking now";
+      //LOG(ERROR) << "Socket " << pipe_name_ << " must be O_NONBLOCK";
+      //return false;
     }
 #endif   // IPC_USES_READWRITE
   } else if (mode_ & MODE_NAMED_FLAG) {

commit 625e95281b53133f6871ebc24b9e3d8a47f09ade

Socketpair of sel_ldr shouldn't be non-blocking
This is a regression issue introduced by c6c34d9d

chrome/renderer/pepper/ppb_nacl_private_impl.cc
@@ -132,7 +132,6 @@ PP_NaClResult LaunchSelLdr(PP_Instance instance,
     DLOG(ERROR) << "WebKit::WebProcess::shared().launchNaCl() failed";
     return PP_NACL_FAILED;
   }
-  ASSERT(fcntl(result_socket.fd, F_SETFL, O_NONBLOCK) != -1);
   ASSERT(fcntl(instance_info.channel_handle.socket.fd, F_SETFL, O_NONBLOCK) != -1);
 
   // Don't save instance_info if channel handle is invalid.